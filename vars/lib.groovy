454096c93dbc5d2232e4200f20076e8ec52ee666
Prashant Kumar <prashantkumar69328@gmail.com>
2023-02-05 16:37:10 +0530

    s

--- vars/lib.groovy
+++ vars/lib.groovy
@@ -7,19 +7,19 @@ def build( giturlrepo ,cmdname)  {
     sh "${cmdname}"
     
     sh "sudo docker build -t webdevprashant/javaapp:${BUILD_NUMBER} ."
-    sh "sudo docker run  -d -p 1222:8080 --name myjavaappdevenv webdevprashant/javaapp:${BUILD_NUMBER}" 
+    sh "sudo docker run  -d -p 1222:8080 --name myjavaapp webdevprashant/javaapp:${BUILD_NUMBER}" 
 }
     
     
 def test(ip , searchitem)    {
-        sh "curl --silent http://${ip}:1223/java-web-app/ |  grep -i ${searchitem}"
+        sh "curl --silent http://${ip}:1222/java-web-app/ |  grep -i ${searchitem}"
 }
     
 def push()                  {
          withCredentials([string(credentialsId: 'Docker_hub_password', variable: 'VAR_FOR_DOCKERPASS')]) {
                     sh "sudo docker login -u webdevprashant -p $VAR_FOR_DOCKERPASS"
                     }
-                    sh "sudo docker push webdevprashant/javaapp-day6:${BUILD_NUMBER}"
+                    sh "sudo docker push webdevprashant/javaapp:${BUILD_NUMBER}"
     }
     
 }
